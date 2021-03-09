fdbaee7e7b56eb83579ed3d1b9fe69ffe93b8ee6
fenggj <feng612266@gmail.com>
2021-03-09 13:43:23 +0800

    finish

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/RallyTheme.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/RallyTheme.kt
@@ -30,6 +30,16 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.R
 
+private val EczarFontFamily = FontFamily(
+    Font(R.font.eczar_regular),
+    Font(R.font.eczar_semibold, FontWeight.SemiBold)
+)
+private val RobotoCondensed = FontFamily(
+    Font(R.font.robotocondensed_regular),
+    Font(R.font.robotocondensed_light, FontWeight.Light),
+    Font(R.font.robotocondensed_bold, FontWeight.Bold)
+)
+
 /**
  * A [MaterialTheme] for Rally.
  */
@@ -46,6 +56,7 @@ fun RallyTheme(content: @Composable () -> Unit) {
     )
 
     val typography = Typography(
+        defaultFontFamily = RobotoCondensed,
         h1 = TextStyle(
             fontWeight = FontWeight.W100,
             fontSize = 96.sp,
@@ -53,6 +64,7 @@ fun RallyTheme(content: @Composable () -> Unit) {
         h2 = TextStyle(
             fontWeight = FontWeight.SemiBold,
             fontSize = 44.sp,
+            fontFamily = EczarFontFamily,
             letterSpacing = 1.5.sp
         ),
         h3 = TextStyle(
@@ -71,6 +83,7 @@ fun RallyTheme(content: @Composable () -> Unit) {
             fontWeight = FontWeight.Normal,
             fontSize = 18.sp,
             lineHeight = 20.sp,
+            fontFamily = EczarFontFamily,
             letterSpacing = 3.sp
         ),
         subtitle1 = TextStyle(
